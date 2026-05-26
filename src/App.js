8b8226efec43b00b323c67222623a49b0cb3968f
Mykola Stepanov <ukrnikolas@gmail.com>
2026-05-26 21:50:52 +0300

    Customize main environment

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="main logo" />
+          <h1 className="App-title">MAIN Environment</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Jenkins CI/CD deployment for the main branch.
         </p>
       </div>
     );
